418a9872e7f6b17b1763069ab5b972cb5c6988ba
Newt Fourie <newt.fourie@entelect.co.za>
2022-08-29 17:14:50 +0200

    Touch up

--- src/app/boolean-yes-no.pipe.ts
+++ src/app/boolean-yes-no.pipe.ts
@@ -5,17 +5,12 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class BooleanYesNoPipe implements PipeTransform {
 
-  transform(value: boolean, customArg: boolean, args?: string[]): string {
-    let append = ''
-    if (args) {
-      append += args[0];
+  transform(value: boolean, humanize: boolean): string {
+    if (humanize) {
+      return value ? `Yes` : `No`
     }
 
-    if (customArg) {
-      return value ? `Yes ${append}` : `No ${append}`
-    }
-
-    return `${value.toString()} ${append}`;
+    return `${value.toString()}`;
   }
 
 }
